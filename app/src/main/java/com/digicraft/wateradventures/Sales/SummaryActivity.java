cf29da54b7199a419d05154c02bb104eba68c733
mac <digicraft786@gmail.com>
2018-07-16 13:58:17 +0500

    Update_16_07_2018

--- app/src/main/java/com/digicraft/wateradventures/Sales/SummaryActivity.java
+++ app/src/main/java/com/digicraft/wateradventures/Sales/SummaryActivity.java
@@ -1,15 +1,23 @@
 package com.digicraft.wateradventures.Sales;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.widget.TextView;
 
 import com.digicraft.wateradventures.R;
 
 public class SummaryActivity extends AppCompatActivity {
+
+    SpannableStringBuilder ssBuilder;
+    TextView termsAndConditions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +26,31 @@ public class SummaryActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        termsAndConditions = findViewById(R.id.terms_and_conditions);
+
+        String text = "By tapping above and placing order, I agree that I have read and agree to be bound by\n" +
+                "Water Adventure's Terms and Conditions and Privacy Statement.";
+        ssBuilder = new SpannableStringBuilder(text);
+
+        ssBuilder.setSpan(
+                new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)),
+                text.indexOf("Terms and Conditions"),
+                text.indexOf("Terms and Conditions") + String.valueOf("Terms and Conditions").length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+
+        ssBuilder.setSpan(
+                new ForegroundColorSpan(getResources().getColor(R.color.colorAccent)),
+                text.indexOf("Privacy Statement"),
+                text.indexOf("Privacy Statement") + String.valueOf("Privacy Statement").length(),
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+
+        termsAndConditions.setText(ssBuilder);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
